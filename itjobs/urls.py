1bc8b311c25a497fd53464279c6d2304e86b8296
Yi Chao <yichaonet@gmail.com>
2013-09-02 12:44:28 +0800

    vendor files ready

--- itjobs/urls.py
+++ itjobs/urls.py
@@ -5,13 +5,5 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'itjobs.views.home', name='home'),
-    # url(r'^itjobs/', include('itjobs.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+  url(r'^jobinfos/', include('jobinfos.urls')),
 )
--- jobinfos/views.py
+++ jobinfos/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from jobinfos.models import JobInfo
+
+def index(request):
+  return render(request, 'jobinfos/index.html')
