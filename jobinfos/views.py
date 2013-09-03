e43db305498471881449dd2b2fa9911130d75615
Chao Yi <chao.yi@hulu.com>
2013-09-03 21:57:20 +0800

    order change

--- jobinfos/views.py
+++ jobinfos/views.py
@@ -5,7 +5,7 @@ from django.http import Http404
 from jobinfos.models import JobInfo
 
 def index(request):
-  jobs_list = JobInfo.objects.order_by('-pub_date')
+  jobs_list = JobInfo.objects.order_by('-pub_date', '-id')
   return render(request, 'jobinfos/index.html', { 'jobs_list': jobs_list })
 
 def detail(request, job_id):
