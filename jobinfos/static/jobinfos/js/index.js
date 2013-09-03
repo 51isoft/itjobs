c38dd174c1f3a1f85fd5d07f27bf4412a7a5d028
Chao Yi <chao.yi@hulu.com>
2013-09-04 00:10:50 +0800

    no intern option

--- jobinfos/static/jobinfos/js/index.js
+++ jobinfos/static/jobinfos/js/index.js
@@ -1,3 +1,19 @@
 jQuery(function($) {
   $(".navbar #index").addClass("active");
+  var job_type=0;
+  function update_table() {
+    $("#jobs tbody tr").each(function() {
+      $(this).show();
+      if (job_type == 1 && $("td:first", this).text().match(/实习|intern/)) $(this).hide();
+    })
+  }
+  $("#job_type button").click(function(){
+    if ($(this).hasClass('active')) return;
+    $(this).parent().children().removeClass('active')
+    $(this).addClass('active')
+    if ($(this).attr('id') == 'job_all') job_type=0;
+    else job_type=1;
+    update_table();
+    return false;
+  })
 });
