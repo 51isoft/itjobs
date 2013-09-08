f9e0bb418e0ceb2091090cc7cf9bfb90ae96a73f
Chao Yi <chao.yi@hulu.com>
2013-09-08 22:36:22 +0800

    source selector

--- jobinfos/static/jobinfos/js/index.js
+++ jobinfos/static/jobinfos/js/index.js
@@ -1,10 +1,14 @@
 jQuery(function($) {
   $(".navbar #index").addClass("active");
   var job_type=0;
+  var job_source=0;
   function update_table() {
     $("#jobs tbody tr").each(function() {
       $(this).show();
       if (job_type == 1 && $("td:first", this).text().match(/实习|intern/i)) $(this).hide();
+      if (job_source == 1 && $("td:nth-child(3)", this).text() != "水木清华BBS") $(this).hide();
+      if (job_source == 2 && $("td:nth-child(3)", this).text() != "北师就业中心") $(this).hide();
+      if (job_source == 3 && $("td:nth-child(3)", this).text() != "大街网") $(this).hide();
     })
   }
   $("#job_type button").click(function(){
@@ -15,5 +19,16 @@ jQuery(function($) {
     else job_type=1;
     update_table();
     return false;
-  })
+  });
+  $("#job_source button").click(function(){
+    if ($(this).hasClass('active')) return;
+    $(this).parent().children().removeClass('active')
+    $(this).addClass('active')
+    if ($(this).attr('id') == 'job_smth') job_source=1;
+    else if ($(this).attr('id') == 'job_bnu') job_source=2;
+    else if ($(this).attr('id') == 'job_dajie') job_source=3;
+    else job_source=0;
+    update_table();
+    return false;
+  });
 });
